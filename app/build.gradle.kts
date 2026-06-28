4be75f557ef43b6a156e02079fb5160a46dcceeb
boris55555 <riku.posti@disroot.org>
2026-06-28 11:12:26 +0300

    Release v1.4: Performance improvements and version bump

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -17,8 +17,8 @@ android {
         applicationId = "com.boris55555.feeds"
         minSdk = 31
         targetSdk = 36
-        versionCode = 3
-        versionName = "1.3"
+        versionCode = 4
+        versionName = "1.4"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
